72866d6a3b049145337258a4f80bbbd14cf1d4ff
Bumtarata <svickovak@gmail.com>
2022-07-13 17:37:48 +0200

    Refactored code in Gui class.

--- base_window.py
+++ base_window.py
@@ -30,11 +30,17 @@ class Gui():
         self.screen = base_window.screen
         self.screen_rect = base_window.screen.get_rect()
         
-        # Create head rect.
+        # Draw the rects.
+        self._draw_rects(self._create_gui_rects())
+        
+    def _create_gui_rects(self):
+        """Creates all basic gui rects."""
+        # Create head rects.
         self.head_rect = pygame.Rect(0, 0, self.settings.screen_width - 30, 
             self.settings.screen_height // 7 + 10)
         self.head_rect.centerx = self.screen_rect.centerx
         self.head_rect.top = 15
+        self.head_rect_color = (150, 150, 150)
         
         self.mines_left_rect = pygame.Rect(
             self.head_rect.top + 10,
@@ -42,14 +48,17 @@ class Gui():
             self.head_rect.width // 3 - 20,
             self.head_rect.height - 20
         )
+        self.mines_left_rect_color = (200, 50, 50)
         
         self.time_rect = pygame.Rect(self.mines_left_rect)
         self.time_rect.right = self.head_rect.right - 5
         self.time_rect.y = self.mines_left_rect.y
+        self.time_rect_color = self.mines_left_rect_color
         
         self.restart_rect = pygame.Rect(0, 0, self.mines_left_rect.height,
             self.mines_left_rect.height)
         self.restart_rect.center = self.head_rect.center
+        self.restart_rect_color = (230, 230, 0)
         
         # Create body rect.
         self.body_rect = pygame.Rect(
@@ -58,10 +67,19 @@ class Gui():
             self.head_rect.width,
             self.screen_rect.height - self.head_rect.height - 45
         )
+        self.body_rect_color = (150, 150, 150)
         
-        # Draw the rects.
-        self.screen.fill((150, 150, 150), rect=self.head_rect)
-        self.screen.fill((150, 150, 150), rect=self.body_rect)
-        self.screen.fill((200, 50, 50), rect=self.mines_left_rect)
-        self.screen.fill((200, 50, 50), rect=self.time_rect)
-        self.screen.fill((230, 230, 0), rect=self.restart_rect)
+        # Return list of rects and its colors tuples.
+        all_basic_gui_rects = [
+            (self.head_rect, self.head_rect_color),
+            (self.mines_left_rect, self.mines_left_rect_color),
+            (self.time_rect, self.time_rect_color),
+            (self.restart_rect, self.restart_rect_color),
+            (self.body_rect, self.body_rect_color),
+        ]
+        return all_basic_gui_rects
+        
+    def _draw_rects(self, all_rects):
+        """Draw given rects with given color."""
+        for one_rect in all_rects:
+            self.screen.fill(one_rect[1], rect=one_rect[0])
